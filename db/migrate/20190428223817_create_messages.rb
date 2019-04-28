804b795b461ba4a93878b98fa224553d2068100a
696b <k.kaitophone@gmail.com>
2019-04-29 08:12:07 +0900

    summary

---
+++ db/migrate/20190428223817_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.text        :body
+      t.string      :image
+      t.references  :user_id
+      t.references  :group_id
+      t.timestamps null: true
+    end
+  end
+end
