6f022cc8d6f6f50b64aaa5ffef7207e1be343fb6
696b <k.kaitophone@gmail.com>
2019-05-06 18:39:53 +0900

    summary

--- db/migrate/20190428225842_create_members.rb
+++ db/migrate/20190428225842_create_members.rb
@@ -1,7 +1,8 @@
-class CreateMembers < ActiveRecord::Migration[5.0]
+class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :members do |t|
-
+    create_table :group_users do |t|
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
