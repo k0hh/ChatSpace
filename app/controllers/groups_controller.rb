7d232e3e92a60a3179865cefe845d0d23cf86064
696b <k.kaitophone@gmail.com>
2019-05-07 17:40:40 +0900

    summary

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,21 @@
 class GroupsController < ApplicationController
-  def new 
+  def index
+
+  end
+  
+  def new
+    @group = Group.new
+    @group.users << current_user
   
   end
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
 
   end
   def edit
@@ -12,4 +25,8 @@ class GroupsController < ApplicationController
   
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name,{:user_ids => []})
+  end
 end
