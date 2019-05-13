351a1604525eaca588d1a3aadec95c2c1c34a711
696b <k.kaitophone@gmail.com>
2019-05-13 09:28:13 +0900

    summary

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,9 +18,9 @@ class GroupsController < ApplicationController
     end
   end
 
-  # def edit
+  def edit
 
-  # end
+  end
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice:'グループを編集しました'
