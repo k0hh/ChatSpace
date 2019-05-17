41569f84e68e77f89423899f84ed62a9922bf03f
696b <k.kaitophone@gmail.com>
2019-05-17 18:12:44 +0900

    ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,14 +2,19 @@ $(function() {
   $("#new_message").on('submit',function(e){
     e.preventDefault();
     var formData = new FormData($(this).get(0));
+    var $form = $(this);
     var url = $(this).attr('action');
     $.ajax({
       url: url,
+      // url: $form.attr('messages'),
+      // type: $form.attr('create'),
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
+      
     })
+    console.log(formData)
   })
 })
