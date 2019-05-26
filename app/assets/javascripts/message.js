e6949b6f7b95ddde56c349c22294258bbe31efd6
696b <k.kaitophone@gmail.com>
2019-05-26 17:00:53 +0900

    "turbolinks:load"を実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,50 +1,52 @@
-$(function() {
-  function buildHTML(message){
-    var image = ""
-    if(message.image !== null) {
-      image = `<img src= ${ message.image } class="lower-message__image" ></img> `
-    }
-    var html = `<div class="message" data-id="${message.id}">
-                <div class="message__top">
-                  <p class="message__member">
-                    ${message.user_name}
-                  </p>
-                  <p class="message__time">
-                    ${message.date}
-                  </p>
-                </div>
-                <div class="message__bottom">
-                  <p class="message__member__message">
-                    ${message.content}
-                  </p>
-                   ${image}
-                </div>
-              </div>`
-  return html;
-  }
-  $("#new_message").on('submit',function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+$(document).on("turbolinks:load", function(){
+  $(function() {
+      function buildHTML(message){
+        var image = ""
+        if(message.image !== null) {
+          image = `<img src= ${ message.image } class="lower-message__image" ></img> `
+        }
+        var html = `<div class="message" data-id="${message.id}">
+                    <div class="message__top">
+                      <p class="message__member">
+                        ${message.user_name}
+                      </p>
+                      <p class="message__time">
+                        ${message.date}
+                      </p>
+                    </div>
+                    <div class="message__bottom">
+                      <p class="message__member__message">
+                        ${message.content}
+                      </p>
+                      ${image}
+                    </div>
+                  </div>`
+      return html;
+      }
+      $("#new_message").on('submit',function(e){
+        e.preventDefault();
+        var formData = new FormData(this);
+        var url = $(this).attr('action')
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.messages').append(html);
+          $('#message_content').val(''); 
+          $('.main__body').animate({scrollTop: $('.main__body')[0].scrollHeight});
+        })
+        .fail(function(){
+          alert('errorです');
+        })
+        
+        return false;
+        // });
+      })
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('#message_content').val(''); 
-      $('.main__body').animate({scrollTop: $('.main__body')[0].scrollHeight});
-    })
-    .fail(function(){
-      alert('errorです');
-    })
-    
-    return false;
-    // });
   })
-})
