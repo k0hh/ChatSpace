2a527e04c683b2ab8fba7314ee35607bf78c5f16
696b <k.kaitophone@gmail.com>
2019-05-27 19:25:49 +0900

    turbolinksの無効化

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,46 +1,44 @@
-$(document).on("turbolinks:load", function(){
+$(function() {
+  function appendUserHTML(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>` 
+    $('#user-search-result').append(html);
+  }
 
-  $(function() {
-    function appendUserHTML(user) {
-      var html = `<div class="chat-group-user clearfix">
-                    <p class="chat-group-user__name">${user.name}</p>
-                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-                  </div>` 
-      $('#user-search-result').append(html);
-    }
+  function appendErrorHTML(msg){
+    var html = `<div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${msg}</p>
+    </div>`
+    $('#user-search-result').append(html);
+  }
 
-    function appendErrorHTML(msg){
-      var html = `<div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">${msg}</p>
-      </div>`
-      $('#user-search-result').append(html);
-    }
+  $("#user-search-field").on('keyup paste', function(){
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data:{keyword: input},
+      dataType: 'json',
+    })
 
-    $("#user-search-field").on('keyup paste', function(){
-      var input = $("#user-search-field").val();
-      $.ajax({
-        type: 'GET',
-        url: '/users',
-        data:{keyword: input},
-        dataType: 'json',
-      })
-
-      .done(function(users){
-        $("#user-search-result").empty();
-        if　(users.length !== 0 ){
-          users.forEach(function(user){
-            appendUserHTML(user);
-          })
-        }
-        else {
-          appendErrorHTML("一致するユーザーが見つかりません");
-        };
-      })
-      .fail(function(){
-        alert('ユーザー検索に失敗しました');
-      });
+    .done(function(users){
+      $("#user-search-result").empty();
+      if　(users.length !== 0 ){
+        users.forEach(function(user){
+          appendUserHTML(user);
+        })
+      }
+      else {
+        appendErrorHTML("一致するユーザーが見つかりません");
+      };
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
     });
   });
+});
 
 
   $(document).on("click", ".chat-group-user__btn",function(){
@@ -59,7 +57,6 @@ $(document).on("turbolinks:load", function(){
     appendMemberHTML(user_id, user_name);
   })
 
-  $(document).on("click",".user-search-remove", function(){
-    $(".js-chat-member").remove();
-  })
+$(document).on("click",".user-search-remove", function(){
+  $(".js-chat-member").remove();
 })
