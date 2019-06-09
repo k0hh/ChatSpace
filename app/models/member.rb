6c50756d4e0bf546f29cc9dbc257d31f3be6d949
696b <k.kaitophone@gmail.com>
2019-06-09 16:08:24 +0900

    membersテーブルを作成

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,9 @@
 class Member < ApplicationRecord
-  belongs_to :group
-  belongs_to :user
-
+  def change
+    create_table :group_users do |t|
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
 end
