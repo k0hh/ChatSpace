f5820fdaaa6658e7a21d58ae0f325a9d26c764e1
696b <k.kaitophone@gmail.com>
2019-06-15 16:37:54 +0900

    インクリメンタルサーチでdeleteキーを押すと全ユーザーが表示される問題の解消

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -22,41 +22,42 @@ $(function() {
       data:{keyword: input},
       dataType: 'json',
     })
-
     .done(function(users){
       $("#user-search-result").empty();
-      if (users.length !== 0 ){ 
-        users.forEach(function(user){
-          appendUserHTML(user);
-        })
-      }
-      else {
-        appendErrorHTML("一致するユーザーが見つかりません");
+      if (input.length !== 0){
+        if (users.length !== 0 ){ 
+          users.forEach(function(user){
+            appendUserHTML(user);
+          })
+        }
+        else {
+          appendErrorHTML("一致するユーザーが見つかりません");
+        };
       };
     })
     .fail(function(){
       alert('ユーザー検索に失敗しました');
-    });
+    })
   });
-});
 
 
-$(document).on("click", ".user-search-add",function(){
-  $("#chat-group-users").val();
-  var user_id = $(this).data('user-id');
-  var user_name = $(this).data('user-name');
-  $(this).parent().remove();
-  appendMemberHTML(user_id, user_name);
+  $(document).on("click", ".user-search-add",function(){
+    $("#chat-group-users").val();
+    var user_id = $(this).data('user-id');
+    var user_name = $(this).data('user-name');
+    $(this).parent().remove();
+    appendMemberHTML(user_id, user_name);
+  })
+  $(document).on("click",".user-search-remove", function(){
+    $(this).parent().remove();
+  })
+
+  function appendMemberHTML(user_id, name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='${user_id}'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    $("#chat-group-users").append(html);
+  }
 })
-$(document).on("click",".user-search-remove", function(){
-  $(this).parent().remove();
-})
-
-function appendMemberHTML(user_id, name){
-  var html = `<div class='chat-group-user clearfix js-chat-member' id='${user_id}'>
-                <input name='group[user_ids][]' type='hidden' value='${user_id}'>
-                <p class='chat-group-user__name'>${name}</p>
-                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-              </div>`
-  $("#chat-group-users").append(html);
-}
