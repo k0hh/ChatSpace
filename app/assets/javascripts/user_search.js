8084561458b0e4ae68bbbe82f73f743301bcefdd
696b <k.kaitophone@gmail.com>
2019-06-15 12:15:03 +0900

    user_search.jsのスペースを変更

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -25,7 +25,7 @@ $(function() {
 
     .done(function(users){
       $("#user-search-result").empty();
-      if　(users.length !== 0 ){ 
+      if (users.length !== 0 ){ 
         users.forEach(function(user){
           appendUserHTML(user);
         })
